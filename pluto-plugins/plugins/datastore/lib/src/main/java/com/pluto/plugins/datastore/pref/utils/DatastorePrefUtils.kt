5978df0fbe5c7fcb4f85813a4388d6b8c6963f5f
srtvprateek <srtv.prateek@gmail.com>
2024-03-31 13:45:01 +0530

    refactor

--- pluto-plugins/plugins/datastore/lib/src/main/java/com/pluto/plugins/datastore/pref/utils/DatastorePrefUtils.kt
+++ pluto-plugins/plugins/datastore/lib/src/main/java/com/pluto/plugins/datastore/pref/utils/DatastorePrefUtils.kt
@@ -6,27 +6,15 @@ import com.pluto.plugins.datastore.pref.PlutoDatastoreWatcher
 import com.pluto.plugins.datastore.pref.PreferenceHolder
 import com.pluto.utilities.list.ListItem
 import com.pluto.utilities.views.keyvalue.KeyValuePairEditMetaData
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 
 internal class DatastorePrefUtils(context: Context) {
 
     private val preferences: Preferences = Preferences(context)
-    private val moshi: Moshi = Moshi.Builder().build()
-    private val moshiAdapter: JsonAdapter<List<String>?> = moshi.adapter(Types.newParameterizedType(List::class.java, String::class.java))
 
     internal var selectedPreferenceFiles: List<PreferenceHolder> = arrayListOf()
-        get() {
-            return preferences.selectedPreferenceFiles?.let {
-                moshiAdapter.fromJson(it)?.map { label -> PlutoDatastoreWatcher.getSource(label) }
-            } ?: run {
-                selectedPreferenceFiles = allPreferenceFiles
-                allPreferenceFiles
-            }
-        }
+        get() = preferences.selectedPreferenceFiles
         set(value) {
-            preferences.selectedPreferenceFiles = moshiAdapter.toJson(value.map { it.name })
+            preferences.selectedPreferenceFiles = value
             field = value
         }
 
--- pluto-plugins/plugins/datastore/lib/src/main/java/com/pluto/plugins/datastore/pref/utils/Preferences.kt
+++ pluto-plugins/plugins/datastore/lib/src/main/java/com/pluto/plugins/datastore/pref/utils/Preferences.kt
@@ -1,14 +1,23 @@
 package com.pluto.plugins.datastore.pref.utils
 
 import android.content.Context
+import com.pluto.plugins.datastore.pref.PlutoDatastoreWatcher
+import com.pluto.plugins.datastore.pref.PreferenceHolder
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 
 internal class Preferences(context: Context) {
 
     private val settingsPrefs by lazy { context.preferences("_pluto_datastore_pref_settings") }
+    private val moshi: Moshi = Moshi.Builder().build()
+    private val moshiAdapter: JsonAdapter<List<String>?> = moshi.adapter(Types.newParameterizedType(List::class.java, String::class.java))
 
-    internal var selectedPreferenceFiles: String?
-        get() = settingsPrefs.getString(SELECTED_PREF_FILE, null)
-        set(value) = settingsPrefs.edit().putString(SELECTED_PREF_FILE, value).apply()
+    internal var selectedPreferenceFiles: List<PreferenceHolder>
+        get() = settingsPrefs.getString(SELECTED_PREF_FILE, null)?.let {
+            moshiAdapter.fromJson(it)?.map { label -> PlutoDatastoreWatcher.getSource(label) }
+        } ?: run { emptyList() }
+        set(value) = settingsPrefs.edit().putString(SELECTED_PREF_FILE, moshiAdapter.toJson(value.map { it.name })).apply()
 
     companion object {
         private const val SELECTED_PREF_FILE = "selected_datastore_pref_file"
